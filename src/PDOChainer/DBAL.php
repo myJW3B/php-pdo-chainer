c7256bd6dc0b42aedfcd1712406465834dfdee3f
JWB <john.skem9@gmail.com>
2024-08-11 17:12:14 -0400

    Update DBAL.php
    I cheated and used ChatGPT to update this code to PHP 8.3, and its compatible with PHP 8.

--- src/PDOChainer/DBAL.php
+++ src/PDOChainer/DBAL.php
@@ -3,8 +3,8 @@
 /**
  * DBAL over PDOChainer.
  *
- * See usage examples in README file.
- * See lincense text in LICENSE file.
+ * See usage examples in the README file.
+ * See license text in the LICENSE file.
  *
  * (c) Evgeniy Udodov <flr.null@gmail.com>
  * (c) John Brittain <jb@jw3b.com>
@@ -17,184 +17,182 @@ namespace PDOChainer;
  */
 class DBAL
 {
+	/**
+	 * PDOChainer link.
+	 *
+	 * @var \PDOChainer\PDOChainer
+	 */
+	private \PDOChainer\PDOChainer $pdo;
 
-    /**
-     * PDOChainer link.
-     *
-     * @var \PDOChainer\PDOChainer
-     */
-    private $pdo;
+	/**
+	 * Default constructor.
+	 *
+	 * @param \PDOChainer\PDOChainer $pdo
+	 */
+	public function __construct(\PDOChainer\PDOChainer $pdo)
+	{
+		$this->pdo = $pdo;
+	}
 
-    /**
-     * Default constructor.
-     *
-     * @param \PDOChainer\PDOChainer $pdo
-     */
-    public function __construct(\PDOChainer\PDOChainer $pdo) {
-        $this->pdo = $pdo;
-    }
+	/**
+	 * Inserts data into the database.
+	 *
+	 * @param string $table The name of the table to insert data into.
+	 * @param array $dataArr The data to be inserted, in the format:
+	 * [
+	 *   ['id', 2, \PDO::PARAM_INT],
+	 *   ['name', 'James', \PDO::PARAM_STR],
+	 * ]
+	 *
+	 * @return int|false The inserted ID or false on failure.
+	 */
+	public function insert(string $table, array $dataArr): int|false
+	{
+		$fields = $params = $values = [];
+		foreach ($dataArr as $data) {
+			$fields[] = "`{$data[0]}`";
+			$params[] = ":{$data[0]}";
+			$values[":{$data[0]}"] = [$data[1], $data[2] ?? \PDO::PARAM_STR];
+		}
 
-    /**
-     * Inserts data into DataBase.
-     *
-     * @param string $table
-     * @param array $data
-     * [
-     *   ['id', 2, \PDO::PARAM_INT],
-     *   ['name', 'James', \PDO::PARAM_STR],
-     * ]
-     *
-     * @return int|false Inserted ID or false
-     */
-    public function insert($table, array $dataArr){
-        $fields = $params = $values = [];
-        foreach ($dataArr as $data) {
-            $fields[] = "`{$data[0]}`";
-            $params[] = ":{$data[0]}";
-            $values[] = [":{$data[0]}", $data[1], (isset($data[2]) ? $data[2] : \PDO::PARAM_STR)];
-        }
+		$fieldsStr = implode(',', $fields);
+		$paramsStr = implode(',', $params);
 
-        $fields = implode(',', $fields);
-        $params = implode(',', $params);
+		$sql = "INSERT INTO `{$table}` ({$fieldsStr}) VALUES ({$paramsStr})";
+		$this->pdo->prepare($sql)?->bindValues($values)?->execute();
+		return (int)$this->pdo->lastInsertId();
+	}
 
-        $sql = "INSERT INTO `{$table}` ({$fields}) VALUES ({$params})";
-        $this->pdo->prepare($sql)->bindValues($values)->execute();
-        return (int) $this->pdo->lastInsertId();
-    }
+	/**
+	 * Updates data in the database.
+	 *
+	 * @param string $table The name of the table to update.
+	 * @param array $dataArr The data to be updated, in the format:
+	 * [
+	 *   ['id', 2, \PDO::PARAM_INT],
+	 *   ['name', 'James', \PDO::PARAM_STR],
+	 * ]
+	 * @param array $whereArr The conditions for updating the data, in the format:
+	 * [
+	 *   ['id', 2, \PDO::PARAM_INT],
+	 * ]
+	 * @param int $limit The maximum number of rows to update.
+	 *
+	 * @return int The number of affected rows.
+	 */
+	public function update(string $table, array $dataArr, array $whereArr = [], int $limit = 1): int
+	{
+		$fields = $values = $where = [];
+		foreach ($dataArr as $data) {
+			$fields[] = "`{$data[0]}` = :{$data[0]}";
+			$values[":{$data[0]}"] = [$data[1], $data[2] ?? \PDO::PARAM_STR];
+		}
+		$i = 0;
+		foreach ($whereArr as $wData) {
+			$i++; // The $i is in there because row wouldn't update with :value already being set above
+			$where[] = "`{$wData[0]}` = :{$wData[0]}{$i}";
+			$values[":{$wData[0]}{$i}"] = [$wData[1], $wData[2] ?? \PDO::PARAM_STR];
+		}
 
-    /**
-     * Updates data in DataBase.
-     *
-     * @param string $table
-     * @param array $dataArr
-     * [
-     *   ['id', 2, \PDO::PARAM_INT],
-     *   ['name', 'James', \PDO::PARAM_STR],
-		 *    ...
-     * ]
-     * @param array $whereArr
-     * [
-     *   ['id', 2, \PDO::PARAM_INT],
-     * ]
-     * @param int $limit
-     *
-     * @return int Affected rows count
-     */
-    public function update($table, array $dataArr, array $whereArr = [], $limit = 1){
-        $fields = $values = $where = [];
-        foreach($dataArr as $data){
-            $fields[] = "`{$data[0]}` = :{$data[0]}";
-            $values[] = [":{$data[0]}", $data[1], (isset($data[2]) ? $data[2] : \PDO::PARAM_STR)];
-        }
-        $i = 0;
-        foreach($whereArr as $wData){
-            $i++; // The $i is in there because row wouldnt update with :value already being set above
-            $where[] = "`{$wData[0]}` = :{$wData[0]}{$i}";
-            $values[] = [":{$wData[0]}{$i}", $wData[1], (isset($wData[2]) ? $wData[2] : \PDO::PARAM_STR)];
-        }
+		$fieldsStr = implode(',', $fields);
+		$whereStr = count($where) ? 'WHERE ' . implode(' AND ', $where) : '';
 
-        $fields = implode(',', $fields);
-        $whereStr = count($where) ? 'WHERE '.implode(' AND ', $where) : '';
+		$sql = "UPDATE `{$table}` SET {$fieldsStr} {$whereStr} LIMIT {$limit}";
+		$this->pdo->prepare($sql)?->bindValues($values)?->execute();
+		return $this->pdo->rowCount();
+	}
 
-        $sql = "UPDATE `{$table}` SET {$fields} {$whereStr} LIMIT {$limit}";
-        $this->pdo->prepare($sql)->bindValues($values)->execute();
-        return $this->pdo->rowCount();
-    }
+	/**
+	 * Removes data from the database.
+	 *
+	 * @param string $table The name of the table to delete data from.
+	 * @param array $dataArr The conditions for deleting the data, in the format:
+	 * [
+	 *   ['id', 2, \PDO::PARAM_INT],
+	 * ]
+	 * @param int $limit The maximum number of rows to delete.
+	 *
+	 * @return int The number of affected rows.
+	 */
+	public function delete(string $table, array $dataArr, int $limit = 1): int
+	{
+		$fields = $values = [];
+		foreach ($dataArr as $data) {
+			$fields[] = "`{$data[0]}` = :{$data[0]}";
+			$values[":{$data[0]}"] = [$data[1], $data[2] ?? \PDO::PARAM_STR];
+		}
 
-    /**
-     * Removes data from DataBase.
-     *
-     * @param string $table
-     * @param array $dataArr
-     * [
-     *   ['id', 2, \PDO::PARAM_INT],
-     *   ['name', 'James', \PDO::PARAM_STR],
-		 *    ...
-     * ]
-     * @param int $limit
-     *
-     * @return int Affected rows count
-     */
-    public function delete($table, array $dataArr, $limit = 1){
-        foreach($dataArr as $data){
-            $fields[] = "`{$data[0]}` = :{$data[0]}";
-            $values[] = [":{$data[0]}", $data[1], (isset($data[2]) ? $data[2] : \PDO::PARAM_STR)];
-        }
+		$fieldsStr = implode(' AND ', $fields);
 
-        $fields = implode(' AND ', $fields);
+		$sql = "DELETE FROM `{$table}` WHERE {$fieldsStr} LIMIT {$limit}";
+		$this->pdo->prepare($sql)?->bindValues($values)?->execute();
+		return $this->pdo->rowCount();
+	}
 
-        $sql = "DELETE FROM `{$table}` WHERE {$fields} LIMIT {$limit}";
-        $this->pdo->prepare($sql)->bindValues($values)->execute();
-        return $this->pdo->rowCount();
-    }
+	/**
+	 * Inserts multiple data rows into the database.
+	 *
+	 * @param string $table The name of the table to insert data into.
+	 * @param array $dataArr The data to be inserted, in the format:
+	 * [
+	 *   [
+	 *     ['id', 2, \PDO::PARAM_INT],
+	 *     ['name', 'James', \PDO::PARAM_STR],
+	 *   ],
+	 *   ...
+	 * ]
+	 *
+	 * @return int|false The last inserted ID or false on failure.
+	 */
+	public function insertMulti(string $table, array $dataArr): int|false
+	{
+		$i = 0;
+		$fields = [];
+		$params = [];
+		$values = [];
+		foreach ($dataArr as $data) {
+			$placeholders = [];
+			foreach ($data as $rowData) {
+				$i++;
+				if (!in_array("`{$rowData[0]}`", $fields)) {
+					$fields[] = "`{$rowData[0]}`";
+				}
+				$placeholders[] = ":{$rowData[0]}{$i}";
+				$values[":{$rowData[0]}{$i}"] = [$rowData[1], $rowData[2] ?? \PDO::PARAM_STR];
+			}
+			$params[] = '(' . implode(',', $placeholders) . ')';
+		}
 
-    /**
-     * Inserts multiple data into DataBase.
-     *
-     * @param string $table
-     * @param array $dataArr
-     * [
-     *   [
-     *     ['id', 2, \PDO::PARAM_INT],
-     *     ['name', 'James', \PDO::PARAM_STR],
-     *   ],
-     *   ...
-     * ]
-     *
-     * @return int|false Last inserted ID or false
-     */
-    public function insertMulti($table, array $dataArr){
-        $i = 0;
-        $fields = array();
-        foreach($dataArr as $data){
-            $placeholders = array();
-            foreach($data as $rowData){
-                $i++;
-                if(!in_array("`{$rowData[0]}`", $fields)) {
-                    $fields[] = "`{$rowData[0]}`";
-                }
-                $placeholders[] = ":{$rowData[0]}{$i}";
-                $values[] = [":{$rowData[0]}{$i}", $rowData[1], (isset($rowData[2]) ? $rowData[2] : \PDO::PARAM_STR)];
-            }
-            $params[] = '(' . implode(',', $placeholders) . ')';
-        }
+		$fieldsStr = implode(',', $fields);
+		$paramsStr = implode(',', $params);
 
-        $fields = implode(',', $fields);
-        $params = implode(',', $params);
+		$sql = "INSERT INTO `{$table}` ({$fieldsStr}) VALUES {$paramsStr}";
+		$this->pdo->prepare($sql)?->bindValues($values)?->execute();
+		return (int)$this->pdo->lastInsertId();
+	}
 
-        $sql = "INSERT INTO `{$table}` ({$fields}) VALUES {$params}";
-        $this->pdo->prepare($sql)->bindValues($values)->execute();
-        return (int) $this->pdo->lastInsertId();
-    }
-
-		/**
-     * Select data from the DataBase.
-     *
-     * @param string $sql 	Full sql statement
-     * @param int $limit		if returning multi rows, put a number grater than 1
-     * @param array $binds
-     * [
-     *   ['id', 2, \PDO::PARAM_INT],
-     *   ['name', 'James', \PDO::PARAM_STR],
-     *   ...
-     * ]
-     *
-     * @return array containing all rows retrieved from the database
-     */
-
-    public function select($sql, $limit = 1, $binds = []){
-        $get = $this->pdo->prepare($sql);
-        if($binds != ''){
-            $get = $get->bindValues($binds);
-        }
-        $get = $get->execute();
-        if($limit > 0){
-            if($limit > 1){
-                $get = $get->fetchAll();
-            } else {
-                $get = $get->fetch();
-            }
-        }
-        return $get;
-    }
+	/**
+	 * Selects data from the database.
+	 *
+	 * @param string $sql The full SQL query to execute.
+	 * @param int $limit If returning multiple rows, specify the number of rows.
+	 * @param array $binds The data to bind to the query, in the format:
+	 * [
+	 *   ['id', 2, \PDO::PARAM_INT],
+	 * ]
+	 *
+	 * @return array The retrieved rows from the database.
+	 */
+	public function select(string $sql, int $limit = 1, array $binds = []): array
+	{
+		$get = $this->pdo->prepare($sql);
+		if (!empty($binds)) {
+			$get?->bindValues($binds);
+		}
+		$get?->execute();
+		if ($limit > 1) {
+			return $get?->fetchAll() ?? [];
+		}
+		return $get?->fetch() ?: [];
+	}
 }
